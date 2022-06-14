a86db7a8773167a46a1a257efc74318d8a9eea98
eckertan <eckertan@uni-marburg.de>
2022-06-14 13:51:10 +0200

    main model and test model: new results and plots

--- Logistic_Model/logistic_model.R
+++ Logistic_Model/logistic_model.R
@@ -5,14 +5,15 @@ library(MuMIn)
 library(optimx)
 
 # read data - auditory
-data_aud <- read.csv("C://Users//annae//Desktop//ChoiceHistory_Psych//Data//Exp1_auditory//exp1_model1_auditory.csv")
+data_aud <- read.csv("C://Users//annae//Desktop//ChoiceHistory_Psych//Data//Exp1_auditory//exp1_prevMotor_prevEvidence.csv")
 
 # read data - visual
-data_vis <- read.csv("C://Users//annae//Desktop//ChoiceHistory_Psych//Data//Exp2_visual//exp2_model1_visual.csv")
+data_vis <- read.csv("C://Users//annae//Desktop//ChoiceHistory_Psych//Data//Exp2_visual//exp2_prevMotor_prevDiff.csv")
 
 # set up model on auditory data...
 # patsy auditory
-patsy_aud <- 'response ~ (1|sbj_id)  + (1|block:sbj_id) + target_z + stimulus_z  + stim_1_z + cue_z * PPS_z * block_type_z + resp_1_z * PPS_z * block_type_z'
+patsy_aud <- 'response ~ (1|sbj_id) + (1|block:sbj_id) + target_z + evidence_z  + stim_1_z + resp_1_z*PPS_z*block_type_z + cue_z*PPS_z*block_type_z'
+#patsy_aud <- 'response ~ (1|sbj_id) + (1|block:sbj_id) + target_z + diff_z  + resp_1_z*PPS_z*block_type_z + cue_z*PPS_z*block_type_z + prev_diff_z*resp_1_z'
 
 # patsy for auditory neutral blocks only
 #patsy_aud <- 'response_z ~ (1|sbj_id) + target_z + stimulus_z + cue_z * PPS_z + stim_1_z + resp_1_z * PPS_z'
@@ -22,14 +23,14 @@ model1_aud <- glmer(patsy_aud, data=data_aud,
                     control=glmerControl(optimizer='optimx', optCtrl=list(method='nlminb')))
 
 # save model in summary text file for later manip. in python
-sink('model_1_auditory_Z.txt')
+sink('model1_aud.txt')
 print(summary(model1_aud))
 sink()
 
 # set up model on visual data... 
 # patsy visual
-patsy_vis <- 'response ~ (1|sbj_id)  + (1|Block:sbj_id) + target_z + diff_z  + stim_1_z + cue_z * PPS_z + resp_1_z * PPS_z '
-
+patsy_vis <- 'response ~ (1|sbj_id)  + (1|Block:sbj_id) + target_z + coherence_z  + stim_1_z + resp_1_z*PPS_z*block_type_z + cue_z*PPS_z*block_type_z'
+#patsy_vis <- 'response ~ (1|sbj_id)  + (1|Block:sbj_id) + target_z + coherence_z  + resp_1_z*PPS_z*block_type_z + cue_z*PPS_z*block_type_z  + prev_coh_z*resp_1_z'
 # patsy visual, neutral blocks only
 #patsy_vis <- 'response ~ (1|sbj_id) + target + coherence + cue * PPS_z + stim_1 + resp_1 * PPS_z'
 
@@ -39,11 +40,11 @@ model1_vis <- glmer(patsy_vis, data=data_vis,
 
 
 # save model summary in text file
-sink('model_1_visual_Z.txt')
+sink('model1_vis.txt')
 print(summary(model1_vis))
 sink()
 
-#r.squaredGLMM(model1_aud)
-#r.squaredGLMM(model1_vis)
+r.squaredGLMM(model1_aud)
+r.squaredGLMM(model1_vis)
 
 
